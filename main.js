dcb50a4fb646efc7a4c5c92538de0bd64ea8b35e
Gustavo Ayala <110442064+ayala66331@users.noreply.github.com>
2023-02-17 09:44:46 -0300

    mejorando el codigo html y la navBar

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ $(document).ready(function(){
     datafn = ajax
     $("#generador").click(function(){
         alert(datafn)
-    })
-
-
-
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-          self.datafn = data;
-        }
-      });
-           
-    
+    })    
 })
-document.getElementById('datos').onclick = function (){
+
+document.getElementById('home').onclick = function (){
   console.log("Capturamos el evento click");
-  document.getElementById('demo').innerHTML = "Nombre:  Cosme Fulanguito, Algo mas para escribir por las moscas que quiera ver como es que se ve el texto cuando la oracion es mas larga de lo que imagino entrara en un explorador con un monitor extra pequeño para poder ver como ose organiza la pagina cuando tengo un texto grante"
+  document.getElementById('main').innerHTML = "Nombre:  Cosme Fulanguito, Algo mas para escribir por las moscas que quiera ver como es que se ve el texto cuando la oracion es mas larga de lo que imagino entrara en un explorador con un monitor extra pequeño para poder ver como ose organiza la pagina cuando tengo un texto grante"
 }
+
+var app = {}
+var miCallBack = function(datos){
+  console.log(datos);
+}
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+  app=data;
+  miCallBack(app);
+  }
+});
+      
